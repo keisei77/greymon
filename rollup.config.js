d072f60bf7bfce34f03af2837ccb1e8adb1698b3
keisei77 <keisei.jia@qq.com>
2020-05-18 22:57:50 +0800

    :package: remove unused module

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 import typescript from 'rollup-plugin-typescript2';
@@ -8,16 +7,12 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      name: 'howLongUntilLunch',
+      name: 'greymon',
       file: pkg.browser,
       format: 'umd',
       sourcemap: true,
     },
-    plugins: [
-      typescript(),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-    ],
+    plugins: [typescript(), commonjs()],
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
@@ -29,8 +24,8 @@ export default [
   {
     input: 'src/index.ts',
     output: [
-      { file: pkg.main, format: 'cjs', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true },
+      { name: 'greymon', file: pkg.main, format: 'cjs', sourcemap: true },
+      { name: 'greymon', file: pkg.module, format: 'es', sourcemap: true },
     ],
     plugins: [typescript()],
   },
