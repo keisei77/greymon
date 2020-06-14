c18eaf9958e2d80a10b480be16babf5c1a2de827
keisei77 <keisei.jia@qq.com>
2020-06-14 20:57:28 +0800

    :construction: support append child to mount element

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,9 +1,15 @@
 type PlayGroundType = '2d' | '3d';
 
-interface PlayGroundOptions {}
+interface PlayGroundOptions {
+  type: PlayGroundType;
+}
 
 export default class playGround {
   private type: PlayGroundType = '2d';
 
-  constructor(options: PlayGroundOptions) {}
+  constructor(mountEl: HTMLElement, options: PlayGroundOptions) {
+    const canvasEl = document.createElement('canvas');
+    const canvas = canvasEl.getContext(options.type);
+    mountEl.appendChild(canvasEl);
+  }
 }
